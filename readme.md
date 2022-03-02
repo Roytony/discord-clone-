78025909e5e6e5e4d58eaf773cad1217635220fd
Arindam242 <creativeab26@gmail.com>
2022-03-02 19:24:46 +0530

    add chat with emoji functionality

--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
 ## Next.js Firebase tailwind TypeScript Discord web app clone
 
 ### i tried to make an app like discord
+
+### Features
+
+- [x] can make servers
+- [x] can make new topics to talk about
+- [x] can chat on topics
+- [ ] users can directly talk to each other
+- [ ] users can create group too
+- [ ] make some useful widgets too for this app (eg: promodaro timer , to do list ) you can set it on for your group or topics member
+- [ ] send media msgs
--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,5 +1,6 @@
-import { addDoc, collection } from 'firebase/firestore'
+import { addDoc, collection, orderBy, serverTimestamp, query } from 'firebase/firestore'
 import React, { useState } from 'react'
+import InputEmoji from 'react-input-emoji'
 import { BiSearch, BiMicrophone, BiPaperPlane } from 'react-icons/bi'
 import { db } from '@src/utils/Firebase'
 import { useCollection } from 'react-firebase-hooks/firestore'
@@ -15,19 +16,18 @@ const Chat = ({ topic }: ChatProps) => {
   const [msg, setMsg] = useState('')
 
   const InputSubmit = async (e) => {
-    if (e.code === 'Enter') {
-      await addDoc(collection(db, `topics/${topic.id}/messages`), {
-        msg,
-        user: '1',
-      })
-      setMsg('')
-    }
+    await addDoc(collection(db, `topics/${topic.id}/messages`), {
+      msg,
+      user: '1',
+      createdAt: serverTimestamp(),
+    })
+    setMsg('')
   }
-  const InputChnage = (e) => {
-    setMsg(e.target.value)
-  }
+  // const InputChnage = (e) => {
+  //   setMsg(e.target.value)
+  // }
 
-  const [value] = useCollection(collection(db, `topics/${topic.id}/messages`))
+  const [value] = useCollection(query(collection(db, `topics/${topic.id}/messages`), orderBy('createdAt', 'desc')))
 
   return (
     <div className="min-w-[1000px] flex flex-col w-full h-screen border-r border-gray-800">
@@ -43,15 +43,26 @@ const Chat = ({ topic }: ChatProps) => {
       </div>
       <div className="w-[95%] flex my-5 items-center rounded-lg h-16  px-4 bg-[#121418] mx-auto">
         <BiSearch className="text-xl text-gray-300 cursor-pointer" />
-        <input
+        {/* <input
           onKeyPress={InputSubmit}
           onChange={InputChnage}
           spellCheck={false}
           placeholder="write a message...."
           className="bg-[#121418] w-full px-4 text-gray-300 h-full rounded-lg focus:outline-none"
+        /> */}
+        <InputEmoji
+          className="bg-[#121418] w-full px-4 text-gray-300 h-full rounded-lg focus:outline-none"
+          value={msg}
+          onChange={setMsg}
+          cleanOnEnter
+          onEnter={InputSubmit}
+          placeholder="Type a message"
         />
-        <BiMicrophone className="text-xl text-gray-300 cursor-pointer" />
-        <BiPaperPlane onClick={InputSubmit} className="text-xl ml-2  text-gray-300 cursor-pointer" />
+        <div className="flex space-x-4">
+          <BiMicrophone className="text-xl text-gray-300 cursor-pointer" />
+
+          <BiPaperPlane onClick={InputSubmit} className="text-xl   text-gray-300 cursor-pointer" />
+        </div>
       </div>
     </div>
   )
